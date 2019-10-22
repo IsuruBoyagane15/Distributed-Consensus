8de87ee79f433b0d9987338a9d0d1c5207f76594
isuruboyagane15 <isuruboyagane15@gmail.com>
2019-10-22 21:16:43 +0530

    Implement two phase commit single round

--- src/main/java/distributed_consensus_se/TwoPhaseCommitMember.java
+++ src/main/java/distributed_consensus_se/TwoPhaseCommitMember.java
@@ -2,56 +2,119 @@ package distributed_consensus_se;
 
 import org.graalvm.polyglot.Value;
 
+import java.util.Random;
 import java.util.UUID;
 
 public class TwoPhaseCommitMember extends ConsensusApplication {
 
+    private  boolean voted;
+    private boolean timeoutHandled;
+
     public TwoPhaseCommitMember(String nodeId, String runtimeJsCode, String evaluationJsCode, String kafkaServerAddress,
-                            String kafkaTopic) {
+                                String kafkaTopic) {
         super(nodeId, runtimeJsCode, evaluationJsCode, kafkaServerAddress, kafkaTopic);
+        this.timeoutHandled = false;
+        this.voted = false;
     }
 
     @Override
     public boolean onReceiving(Value evaluationOutput) {
-        return !evaluationOutput.getMember("coordinatorElected").isNull();
+        DistributedConsensus dcf = DistributedConsensus.getDistributeConsensus(this);
+        if (evaluationOutput.getMember("coordinatorElected").toString().equals(this.getNodeId())){
+            if (!timeoutHandled){
+                try {
+                    System.out.println("I AM THE COORDINATOR");
+                    Thread.sleep(8000);
+                    System.out.println("TIME IS UP");
+                    this.timeoutHandled = true;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                dcf.writeACommand("timeout = true;");
+                return false;
+            }
+            else if(evaluationOutput.getMember("commitOrAbort").isNull()){
+                if (evaluationOutput.getMember("votesResult").isBoolean()){
+                    if (evaluationOutput.getMember("votesResult").asBoolean()) {
+                        dcf.writeACommand("result.commitOrAbort = true;");
+                    }
+                    else if(!evaluationOutput.getMember("votesResult").asBoolean()){
+                        dcf.writeACommand("result.commitOrAbort = false;");
+                    }
+                }
+                return false;
+            }
+            else{
+                System.out.println("FINISHING");
+                dcf.writeACommand("participants = [];timeout = false;result = {coordinatorElected: null, votesResult:null, commitOrAbort:null};");
+                return true;
+            }
+        }
+        else{
+            if (!voted){
+                System.out.println("I AM A PARTICIPANT");
+
+                Random random = new Random();
+                boolean canCommit = random.nextBoolean();
+                dcf.writeACommand("voteForCommit(\""+ getNodeId() + "\"," + canCommit + ");");
+                voted = true;
+                return false;
+            }
+            else{
+                return !evaluationOutput.getMember("commitOrAbort").isNull();
+            }
+        }
     }
 
     @Override
     public void commitAgreedValue(Value evaluationOutput) {
-        if(evaluationOutput.getMember("coordinatorElected").toString().equals(getNodeId())){
-            System.out.println("I am the cordinator");
+        DistributedConsensus dcf = DistributedConsensus.getDistributeConsensus(this);
+        dcf.setTerminate(true);
+        if(evaluationOutput.getMember("commitOrAbort").asBoolean()){
+            System.out.println("committed");
         }
         else{
-            System.out.println("Not the cordinator");
-
+            System.out.println("Did not commit");
         }
     }
 
     public static void twoPhaseCommit(String nodeId, String kafkaServerAddress, String kafkaTopic){
-        TwoPhaseCommitMember twoPCParticipant = new TwoPhaseCommitMember(nodeId,                 "participants = [];result = {coordinatorElected: null, coordinatorRequested:null, votesResult:null, commitOrAbort:null};",
-                    "result" ,
+        TwoPhaseCommitMember twoPCParticipant = new TwoPhaseCommitMember(nodeId,
+                "participants = [];timeout = false;result = {coordinatorElected: null, votesResult:null, commitOrAbort:null};" +
+                "function voteForCommit( id, v ) {" +
+                "   for (var i in participants) {" +
+                "     if (participants[i].participant == id) {" +
+                "        participants[i].vote = v;" +
+                "        break;" +
+                "     }" +
+                "   }" +
+                "};"
+                ,
 
-//                "if (participantResponses.some(response => response.node == \"" + coordinatorId + "\" && response.vote === true)){" +
-//                        "result.coordinatorRequested = true;" +
-//                        "if(participantResponses.length == " + instanceCount + "){" +
-//                        "if(participantResponses.every(response  => response.vote == true)){" +
-//                        "result.votesResult = true;" +
-//                        "}" +
-//                        "else{" +
-//                        "result.votesResult = false;" +
-//                        "}" +
-//                        "}" +
-//                        "}" +
-//                        "result",
+        "if(result.coordinatorElected != null){" +
+                "    if(timeout == true){" +
+                "        if (!participants.some(participant => participant.vote == null)){" +
+                "            if(participants.every(participant  => participant.vote == true)){" +
+                "                result.votesResult = true;" +
+                "            }" +
+                "            else{" +
+                "                result.votesResult = false;" +
+                "            }" +
+                "        }   " +
+                "    }" +
+                "}" +
+                "result" ,
+
                 kafkaServerAddress, kafkaTopic);
 
         DistributedConsensus dcf = DistributedConsensus.getDistributeConsensus(twoPCParticipant);
         dcf.start();
-        dcf.writeACommand("if (result.coordinatorElected == null){" +
+        dcf.writeACommand(
+                "if (result.coordinatorElected == null){" +
                 "    result.coordinatorElected = \"" + twoPCParticipant.getNodeId() +"\";" +
                 "}" +
-                "else{" +
-                "participants.push({participant:\""+ twoPCParticipant.getNodeId() + "\",vote: null});" +
+                "else if (timeout == false){" +
+                    "participants.push({participant:\""+ twoPCParticipant.getNodeId() + "\",vote: null});" +
                 "}");
     }
     public static void main(String args[]){
